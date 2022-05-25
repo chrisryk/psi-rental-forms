c89bd96dda1cde8d901c4776b8b5ebd446b2cc63
Krzysztof Rykowski <rykowski.krzysztof@gmail.com>
2022-05-25 15:52:49 +0200

    fix: customer delete connected records

--- UserControls/UserControlCustomerManager.cs
+++ UserControls/UserControlCustomerManager.cs
@@ -1,7 +1,9 @@
 ﻿using CarRental.Forms;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity.Core;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -9,10 +11,13 @@ namespace CarRental
 {
     public partial class UserControlCustomerManager : UserControl
     {
+        private event Action<IEnumerable> DeleteCustomerClicked = (c) => { };
         public static bool AddEditIsOpen { get; set; }
         public UserControlCustomerManager()
         {
             InitializeComponent();
+
+            DeleteCustomerClicked += DeleteData;
             AddEditIsOpen = false;
         }
 
@@ -37,10 +42,16 @@ namespace CarRental
                 dgvCustomers.DataSource = customersData.ToList();
                 dgvCustomers.Columns[0].Visible = false;
             }
+            catch (EntityException ex)
+            {
+                MessageBox.Show("Database connection failed", "Alert!");
+                MessageBox.Show(ex.StackTrace, "Info!");
+            }
             catch (Exception ex)
             {
-                MessageBox.Show("Connection to database failed.", "Alert!");
+                MessageBox.Show("An error occured.", "Alert!");
                 MessageBox.Show(ex.StackTrace, "Info!");
+                throw;
             }
         }
 
@@ -96,13 +107,38 @@ namespace CarRental
                 return;
             }
 
-            foreach (var item in data)
+            DeleteCustomerClicked.Invoke(data);
+        }
+        private void DeleteData(IEnumerable data)
+        {
+            foreach (int id in data)
             {
-                var customerToDelete = RentalDatabase.DB.Customers.Where(c => c.id == item).FirstOrDefault();
+                var invoicesToDelete = from i in RentalDatabase.DB.Invoices
+                                       join r in RentalDatabase.DB.Rents
+                                       on i.rent_id equals r.id
+                                       join c in RentalDatabase.DB.Customers
+                                       on r.customer_id equals c.id
+                                       where c.id == id
+                                       select i;
+
+                foreach (var invoice in invoicesToDelete)
+                {
+                    RentalDatabase.DB.Invoices.Remove(invoice);
+                }
+
+                var rentsToDelete = RentalDatabase.DB.Rents.Where(r => r.customer_id == id);
+
+                foreach (var rent in rentsToDelete)
+                {
+                    RentalDatabase.DB.Rents.Remove(rent);
+                }
+
+                var customerToDelete = RentalDatabase.DB.Customers.Where(c => c.id == id).FirstOrDefault();
                 RentalDatabase.DB.Customers.Remove(customerToDelete);
             }
 
             RentalDatabase.DB.SaveChanges();
+            MessageBox.Show("Data deleted.", "Info!");
             btnSearchCustomer.PerformClick();
         }
         private IEnumerable<int> GetIndexes(DataGridViewSelectedRowCollection data)
